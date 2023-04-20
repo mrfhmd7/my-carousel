82b6bc8b88f60b20d862b33a35958b23cc7c1df4
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-21 00:29:02 +0600

    finished

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+const images = [
+     'images/pic-1.jpg',
+     'images/pic-2.jpg',
+     'images/pic-3.jpg',
+     'images/pic-4.jpg',
+     'images/pic-5.jpg',
+     'images/pic-6.jpg',
+     'images/pic-7.jpg',
+     'images/pic-8.jpg',
+     'images/pic-9.jpg'
+];
+
+let imageIndex = 0;
+const imgEl = document.getElementById('slider-img');
+setInterval(() => {
+     if (imageIndex === images.length) {
+          imageIndex = 0;
+     }
+     const imgUrl = images[imageIndex];
+     // console.log(imageIndex, imgUrl);
+     imgEl.setAttribute('src', imgUrl)
+     imageIndex++;
+}, 1500);
